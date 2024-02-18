7f85c5dd3846748e8980c7fd56645e4561a9d852
lynxx007 <luthfirizky7@gmail.com>
2024-02-18 22:02:33 +0700

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "i.imgur.com",
-        port: "",
-        pathname: "/**",
-      },
       {
         protocol: "https",
         hostname: "th.bing.com",

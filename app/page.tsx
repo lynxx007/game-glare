9d806db4e4cf14ae7f78fa852b5e69ce971476f9
lynxx007 <luthfirizky7@gmail.com>
2024-02-19 15:51:32 +0700

    refractor

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,52 @@
-import { ArticleCard, ArticleCard2 } from "@/components/article-card";
+import { ArticleCard } from "@/components/article-card";
 import { mongodb } from "@/lib/mongodb";
+import Link from "next/link";
 
-async function getArticles() {
-  const db = mongodb.db("game-glare");
-  const articles = await db.collection("articles").find().toArray();
-
-  return articles;
+async function getData() {
+  const db = mongodb.db(process.env.DB_NAME);
+  const data = await db
+    .collection("articles")
+    .find()
+    .limit(5)
+    .sort({ _id: -1 })
+    .toArray();
+  return data;
 }
-
 export default async function Home() {
-  const articles = await getArticles();
-
+  const data = await getData();
+  const firstItem = data[0];
+  const secondItem = data[1];
+  const thirdItem = data[2];
+  const fourthItem = data[3];
+  const fifthItem = data[4];
   return (
-    <div className="grid grid-cols-1 px-3 gap-4 h-fit md:grid-cols-2 md:px-12">
-      {articles.map((article) => (
-        <ArticleCard2
-          key={article._id.toString()}
-          src={article.image}
-          title={article.title}
-        />
-      ))}
-      <div className="grid grid-cols-2 gap-4">
-        <ArticleCard />
-        <ArticleCard />
-        <ArticleCard />
-        <ArticleCard />
+    <div className="grid grid-cols-1 gap-4 md:grid-cols-3 md:h-98">
+      <div className="grid grid-rows-1 gap-4 md:grid-rows-2">
+        <Link href={`/article/${secondItem._id}`}>
+          <div className="rounded-md border-2 h-48">
+            <ArticleCard src={secondItem.image} title={secondItem.title} />
+          </div>
+        </Link>
+        <div className="rounded-md border-2 h-48">
+          <ArticleCard src={fifthItem.image} title={fifthItem.title} />
+        </div>
+      </div>
+      <Link href={`/article/${firstItem._id}`}>
+        <div className="rounded-md border-2 h-48 md:h-full">
+          <ArticleCard src={firstItem.image} title={firstItem.title} />
+        </div>
+      </Link>
+      <div className="grid grid-rows-1 gap-4 md:grid-rows-2">
+        <Link href={`/article/${thirdItem._id}`}>
+          <div className="rounded-md border-2 h-48">
+            <ArticleCard src={thirdItem.image} title={thirdItem.title} />
+          </div>
+        </Link>
+        <Link href={`/article/${fourthItem._id}`}>
+          <div className="rounded-md border-2 h-48">
+            <ArticleCard src={fourthItem.image} title={fourthItem.title} />
+          </div>
+        </Link>
       </div>
     </div>
   );
--- components/article-card.tsx
+++ components/article-card.tsx
@@ -1,42 +1,22 @@
 import Image from "next/image";
-import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Card } from "./ui/card";
 
-export const ArticleCard = () => {
-  return (
-    <Card className="rounded-3xl max-w-[300px] w-full relative">
-      <Image
-        src={"https://i.imgur.com/Cij5vdL.png"}
-        width={545}
-        height={300}
-        alt="Game"
-        className="rounded-3xl object-cover w-full"
-      />
-      <div className="absolute inset-0 bg-black bg-opacity-50 flex items-end justify-start p-2 rounded-3xl md:p-4">
-        <p className="text-white text-xs font-bold tracking-tight leading-tight md:text-lg">
-          Game Glare
-        </p>
-      </div>
-    </Card>
-  );
-};
-
-interface ArticleCard2Props {
+interface ArticleCardProps {
   src: string;
   title: string;
 }
 
-export const ArticleCard2 = ({ src, title }: ArticleCard2Props) => {
+export const ArticleCard = ({ src, title }: ArticleCardProps) => {
   return (
-    <Card className="rounded-3xl relative">
+    <Card className="h-full relative">
       <Image
         src={src}
         height={200}
-        width={545}
+        width={200}
         alt={title}
-        className="rounded-3xl object-cover h-full w-full"
+        className="rounded-md object-cover w-full h-full"
       />
-      <div className="absolute inset-0 bg-black bg-opacity-50 flex items-end justify-start p-4 rounded-3xl">
+      <div className="absolute inset-0 bg-black bg-opacity-10 flex items-end justify-start p-4 rounded-3xl">
         <p className="text-white text-lg font-bold tracking-tight leading-tight">
           {title}
         </p>

29e7494bc4daf278fb4dde72d8957912c1656716
lynxx007 <luthfirizky7@gmail.com>
2024-02-24 20:17:59 +0700

    Add images and truncate text for genre pages (generated by blackbox.ai )

--- app/genre/[...genre]/page.tsx
+++ app/genre/[...genre]/page.tsx
@@ -1,4 +1,5 @@
 import { mongodb } from "@/lib/mongodb";
+import Image from "next/image";
 
 async function getData(genre: string) {
   const db = mongodb.db(process.env.DB_NAME);
@@ -12,12 +13,29 @@ export default async function Genre({
   params: { genre: string[] };
 }) {
   const data = await getData(params.genre[0]);
-
+  const showCharacters = 250;
   return (
-    <div>
+    <div className="flex flex-col space-y-4">
       {data.length > 0 ? (
         data.map((article) => (
-          <div key={article._id.toString()}>{article.title}</div>
+          <div
+            key={article._id.toString()}
+            className="h-40 w-full border-2 rounded-md shadow-md flex"
+          >
+            <Image
+              src={article.image}
+              width={200}
+              height={200}
+              className="rounded-md object-cover h-full"
+              alt={article.title}
+            />
+            <div className="p-4 flex-1">
+              <h1 className="text-xl font-bold">{article.title}</h1>
+              <p className="text-sm p-2">
+                {article.text.slice(0, showCharacters)}...
+              </p>
+            </div>
+          </div>
         ))
       ) : (
         <h1>No data</h1>
